f2a0a372f77f411132ebfd2c9f74eced69349098
karan819381 <karan819381@gmail.com>
2025-06-30 15:47:47 +0530

    "learning C"

---
+++ calculation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    int sum = num1 + num2;
+    int subtract = num1 - num2;
+    int multiplication = num1 * num2;
+    int division = num1 / num2;
+
+    printf("The sum is: %d\n", sum);
+    printf("The subtract is: %d\n", subtract);
+    printf("The multiplication is: %d\n", multiplication);
+    printf("The division is: %d\n", division);
+    return 0;
+}
