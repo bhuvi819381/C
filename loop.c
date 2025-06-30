32dd65dabb0fab4464484e3f4402a098e1b90937
karan819381 <karan819381@gmail.com>
2025-06-30 16:21:23 +0530

    loop

---
+++ loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    for (int i = 0; i <=10 ; i++)
+    {
+        printf("%d\n", i);
+    }
+
+
+    for(int i = 10; i >= 0; i--){
+        printf("%d\n", i);
+    }
+    
+
+
+
+    return 0;
+}
